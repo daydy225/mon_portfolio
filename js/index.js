7e53c145783118bc44af104ab1f789108d0feb18
daydy225 <dabelydadi@gmail.com>
2021-09-20 00:51:31 +0000

    mon portfolio template

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-const navToggle = document.querySelector('nav_toggle');
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav_link');
+
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
-});
+}); 
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      document.body.classList.remove('nav-open');
+    })
+})
