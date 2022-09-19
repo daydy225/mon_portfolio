3cc7691b1e12de4a7ebc9bfa760fed8a06ed88bb
daydy225 <dabelydadi@gmail.com>
2022-09-19 19:18:43 +0000

    mon portfolio Ajout new CV 2022

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav_link');
-
+const navToggle = document.querySelector('.nav-toggle')
+const navLinks = document.querySelectorAll('.nav_link')
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-}); 
+  document.body.classList.toggle('nav-open')
+})
 
 navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      document.body.classList.remove('nav-open');
-    })
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open')
+  })
 })
